51632d456466f09f78a1a44256f63dc9d2b383a6
lihao57 <lihao57@baidu.com>
2024-02-27 17:41:20 +0800

    [Feature] add MixVisionTransformer backbone

--- ppseg/backbones/__init__.py
+++ ppseg/backbones/__init__.py
@@ -5,3 +5,11 @@ from .resnet_vd import (
     ResNet101_vd,
     ResNet152_vd,
 )
+from .mix_transformer import (
+    MixVisionTransformer_B0,
+    MixVisionTransformer_B1,
+    MixVisionTransformer_B2,
+    MixVisionTransformer_B3,
+    MixVisionTransformer_B4,
+    MixVisionTransformer_B5,
+)
